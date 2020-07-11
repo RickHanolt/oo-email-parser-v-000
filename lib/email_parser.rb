079f26604c15ddd41466e67244392920e323e27d
Richard Hanolt <rhanolt@gmail.com>
2020-07-11 09:06:02 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,17 +3,3 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 require 'pry'
-
-class EmailAddressParser
-  attr_accessor :email_addresses
-
-  def initialize(email_addresses)
-    @email_addresses = email_addresses
-  end
-
-  def parse
-    temp_parsed = @email_addresses.split(/,+\s?|\s/)
-    @email_addresses = temp_parsed & temp_parsed
-  end
-
-end
