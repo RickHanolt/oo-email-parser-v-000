e93c1e760b7aded9fffe69146c52b53d15583257
Richard Hanolt <rhanolt@gmail.com>
2020-07-11 09:16:30 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -6,13 +6,13 @@ require 'pry'
 
 class EmailAddressParser
   attr_accessor :email_addresses
-  
+
   def initialize(email_addresses)
     @email_addresses = email_addresses
   end
 
   def parse
-    @email_addresses = email_addresses.split(" ")
+    @email_addresses = email_addresses.split(",")
     binding.pry
   end
 end
